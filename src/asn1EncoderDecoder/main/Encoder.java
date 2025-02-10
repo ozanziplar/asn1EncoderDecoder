69b27dc8d8815fc7c3eb4acd9ea5cd573c07c64d
Ozan Zıplar <ozanziplar@gmail.com>
2025-02-10 23:20:18 +0300

    -added message

--- src/asn1EncoderDecoder/main/Encoder.java
+++ src/asn1EncoderDecoder/main/Encoder.java
@@ -1,8 +1,10 @@
 package asn1EncoderDecoder.main;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
-import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.*;
 import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.util.encoders.Hex;
@@ -11,9 +13,21 @@ public class Encoder {
 	private final StringBuffer buff = new StringBuffer();
 
 	protected byte[] encode(byte[] hexInput) {
+		ASN1Sequence seq = new DERSequence(new ASN1Encodable[] {
+				new DERSequence(
+						new ASN1Encodable[] { new ASN1Integer(5), new ASN1Enumerated(1), new ASN1Enumerated(17) }),
+				new DERSequence(new ASN1Encodable[] {}) });
+	
+		// PER unaligned kodlama
+//		PerEncoding perEncoding = new PerEncoding();
+		
+		
+		
 		byte[] encodedBytes = new byte[0];
 		
 		try {
+			System.out.println("ggg:  " + Arrays.toString(seq.getEncoded()));
+			
 			// ASN.1 structure
 			ASN1EncodableVector vector = new ASN1EncodableVector();
 			vector.add(new DEROctetString(hexInput)); // Convert Byte array to ASN.1 Octet String
